9491841274c46e1f9146e814f8dacab119ef8b1f
Ian HERBERT <i.herbert@younup.fr>
2024-05-06 16:24:40 +0200

    regroup endpoints

--- src/api/routes/routeRoutes.ts
+++ src/api/routes/routeRoutes.ts
@@ -5,7 +5,7 @@ import { findRoutesFromLocation } from "../controllers/locationControllers";
 const departureRouter = Router({});
 
 departureRouter.get("/stop/:stopId", getRoutesForStop);
-departureRouter.get("", getRoutesForDay);
 departureRouter.get("/location/:locationId", findRoutesFromLocation)
+departureRouter.get("", getRoutesForDay);
 
 export default departureRouter; 
